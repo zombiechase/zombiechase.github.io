7c10a3567465e5556eff36903cccc6e3aac759cf
cornellimartin <cor16006@byui.edu>
2019-10-21 21:07:48 -0300

    More commented lines

--- scripts.js
+++ scripts.js
@@ -30,12 +30,12 @@ document.getElementById("option2").innerHTML = storyObject.choice2.label;
 function option1() {
 	var newOption = storyObject.choice1.newPageID;
 	storyObject.story = myData[newOption].story;
-	storyObject.image = myData[newOption].imageURL;
+	/*storyObject.image = myData[newOption].imageURL;*/
 	storyObject.choice1 = myData[newOption].choice1;
 	storyObject.choice2 = myData[newOption].choice2;
 
 	document.getElementById("story").innerHTML = storyObject.story;
-	document.getElementById("image").innerHTML = storyObject.image;
+	/*document.getElementById("image").innerHTML = storyObject.image;*/
 	document.getElementById("option1").innerHTML = storyObject.choice1.label;
 	document.getElementById("option2").innerHTML = storyObject.choice2.label;
 }
@@ -43,12 +43,12 @@ function option1() {
 function option2() {
 	var newOption = storyObject.choice2.newPageID;
 	storyObject.story = myData[newOption].story;
-	storyObject.image = myData[newOption].imageURL;
+	/*storyObject.image = myData[newOption].imageURL;*/
 	storyObject.choice1 = myData[newOption].choice1;
 	storyObject.choice2 = myData[newOption].choice2;
 
 	document.getElementById("story").innerHTML = storyObject.story;
-	document.getElementById("image").innerHTML = storyObject.image;
+	/*document.getElementById("image").innerHTML = storyObject.image;*/
 	document.getElementById("option1").innerHTML = storyObject.choice1.label;
 	document.getElementById("option2").innerHTML = storyObject.choice2.label;
 }
