99633177508cfe00a58e56f0aeadb6df8ffcadf9
cornellimartin <cor16006@byui.edu>
2019-10-21 22:27:45 -0300

    Added comments to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/*AUTOPLAY OF SOUNDS. NEED TO DISCUSS, THIS IS TEMPORAL*/
 var sounds = new Array(new Audio("/audio/1_callout.m4a"), new Audio("/audio/17_yes.m4a"), new Audio("/audio/18_no.m4a"));
 var i = -1;
 playSnd();
@@ -10,16 +11,20 @@ function playSnd() {
 }
 
 var storyObject = {
+	id: "", /*ADDED TO IDENTIFY STORY ENDINGS*/
 	story: "",
 	image: "",
 	choice1: { newPageID: 0, label: "" },
 	choice2: { newPageID: 0, label: "" }
 };
 
+storyObject.id = myData[0].id;
 storyObject.story = myData[0].story;
 storyObject.image = myData[0].imageURL;
 storyObject.choice1 = myData[0].choice1;
 storyObject.choice2 = myData[0].choice2;
+
+/*COMMENTED OUT IMAGES BECAUSE I NEED TO UNDERSTAND THAT AND THEY ARE STOPING OPTIONS FROM WORKING*/
 
 document.getElementById("story").innerHTML = storyObject.story;
 /*document.getElementById("image").innerHTML = storyObject.image;*/
@@ -29,6 +34,7 @@ document.getElementById("option2").innerHTML = storyObject.choice2.label;
 
 function option1() {
 	var newOption = storyObject.choice1.newPageID;
+	storyObject.id = myData[newOption].id;
 	storyObject.story = myData[newOption].story;
 	/*storyObject.image = myData[newOption].imageURL;*/
 	storyObject.choice1 = myData[newOption].choice1;
@@ -42,6 +48,7 @@ function option1() {
 
 function option2() {
 	var newOption = storyObject.choice2.newPageID;
+	storyObject.id = myData[newOption].id;
 	storyObject.story = myData[newOption].story;
 	/*storyObject.image = myData[newOption].imageURL;*/
 	storyObject.choice1 = myData[newOption].choice1;
@@ -53,3 +60,7 @@ function option2() {
 	document.getElementById("option2").innerHTML = storyObject.choice2.label;
 }
 
+/*TRYING TO SHOW BUTTON WHEN WE GET TO AN ENDING. NOT WORKING AS OF NOW*/
+if (storyObject.id == 3 || storyObject.id == 4 || storyObject.id == 6 || storyObject.id == 9 || storyObject.id == 10 || storyObject.id == 12 || storyObject.id == 13) {
+	document.getElementById("reload").style.display = block;
+}
